2f69ac02a012a8f5a2ae86ebeabd4293ca9e5bad
Pravin Pagare <pravinpgr9@gmail.com>
2025-03-27 18:07:31 +0530

    Updated the video Link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -38,7 +38,7 @@ const Hero = () => {
           loop
           playsInline
         >
-          <source src="https://assets.mixkit.co/videos/preview/mixkit-aerial-view-of-mount-kilimanjaro-40662-large.mp4" type="video/mp4" />
+          <source src="https://assets.mixkit.co/videos/43129/43129-720.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <div className="absolute inset-0 bg-black/50" />
